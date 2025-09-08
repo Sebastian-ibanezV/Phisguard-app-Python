297cf0b6a99c82273ec9c9d3faeb91c9f09d6771
Sebastian-ibanezV <sebastiannew2019@gmail.com>
2025-09-08 19:10:44 -0400

    Eliminar archivo no necesario

--- Search_test.py
+++
@@ -1,9 +0,0 @@
-from search import search_urls
-
-keywords = ["huascar", "Adidas", "Sebastian"]
-
-for keyword in keywords:
-    print(f"\nBuscando URLs para: {keyword}")
-    urls = search_urls(keyword)
-    for i, url in enumerate(urls, start=1):
-        print(f"{i}. {url}")
